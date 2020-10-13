f0cfb099e7c56f7d718e4443ca9839dcc30ae926
DannyS-stack <Danny_Spaans@live.nl>
2020-10-13 10:17:08 +0200

    made models and migrations

---
+++ migrations/20201013081021-create-user.js
@@ -0,0 +1,36 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      phone: {
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
---
+++ models/participant.js
@@ -0,0 +1,25 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class participant extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  participant.init(
+    {
+      userId: DataTypes.INTEGER,
+      groupId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "participant",
+    }
+  );
+  return participant;
+};
